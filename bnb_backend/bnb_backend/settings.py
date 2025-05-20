ae1781fb0fac6b7ab42d779c1f664a8ffb7552bb
yousef <yousef.rezaei9@gmail.com>
2025-05-20 02:04:13 +0200

    Set settings and url file for send image files

--- bnb_backend/bnb_backend/settings.py
+++ bnb_backend/bnb_backend/settings.py
@@ -51,7 +51,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 REST_AUTH = {
     "USE_JWT": True,
     "JWT_AUTH_HTTPONLY": False
--- bnb_backend/bnb_backend/urls.py
+++ bnb_backend/bnb_backend/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/',include('property.urls'))
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+# STATIC_URL = 'static/'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
