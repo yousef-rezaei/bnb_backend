b8d880b33168bb9c1d930bbf9d9026ce3f3bef6c
yousef <yousef.rezaei9@gmail.com>
2025-07-03 19:15:17 +0200

    send and receive messages

--- bnb_backend/chat/consumers.py
+++ bnb_backend/chat/consumers.py
@@ -17,9 +17,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def disconnect(self):
+    async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        conversation_id = data['data']['conversation_id']
+        sent_to_id = data['data']['sent_to_id']
+        name = data['data']['name']
+        body = data['data']['body']
+        
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'body': body,
+                'name': name
+            }
+        )
+        
+    #Sending messages
+    async def chat_message(self, event):
+        body = event['body']
+        name = event['name']
+
+        await self.send(text_data=json.dumps({
+            'body': body,
+            'name': name
+        }))
+        
