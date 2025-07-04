f9c7f026ef245b25b28d59a68e6a8f3b30794d5b
yousef <yousef.rezaei9@gmail.com>
2025-07-04 15:08:35 +0200

    Start new conversation

--- bnb_backend/chat/api.py
+++ bnb_backend/chat/api.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from .models import Conversation
 from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer   
 
+from useraccount.models import User
+
 @api_view(['GET'])
 def conversation_list(request):
     serializer = ConversationListSerializer(request.user.conversations.all(), many=True)
@@ -22,3 +24,19 @@ def conversation_detail(request, pk):
         'messages': messages_serializer.data
         }, safe=False)
     
+
+@api_view(['GET'])
+def conversation_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in =[user_id]).filter(users__in=[request.user.id])
+    
+    if conversations.count() > 0:
+        conversations = conversations.first()
+        
+        return JsonResponse({'success': True, 'conversation_id': conversations.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+        
+        return JsonResponse({'success': True, 'conversation_id': conversations.id})
--- bnb_backend/chat/urls.py
+++ bnb_backend/chat/urls.py
@@ -4,5 +4,6 @@ from . import api
 
 urlpatterns = [
     path('', api.conversation_list, name='api_conversation-list'),
+    path('start/<uuid:user_id>/', api.conversation_start, name='api_conversations-start'),
     path('<uuid:pk>/', api.conversation_detail, name='api_conversation-detail'),
     ]
