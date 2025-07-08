f6f45e14181063b302b72457f03aa4b7579e8289
yousef <yousef.rezaei9@gmail.com>
2025-07-08 02:27:56 +0200

    last changes

--- bnb_backend/chat/routing.py
+++ bnb_backend/chat/routing.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
     # path('ws/notification/', consumers.NotificationConsumer.as_asgi()),
     # path('ws/notification/<int:property_id>/', consumers.PropertyNotificationConsumer.as_asgi()),
 ]
